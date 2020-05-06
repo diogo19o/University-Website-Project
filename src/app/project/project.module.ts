5664b428ec3c42e3f90af9b3052ed749c640db68
perdigaop <pedro.perdigao@cgi.com>
2020-05-06 21:31:32 +0100

    Added Project Modules Added Routing

--- src/app/project/project.module.ts
+++ src/app/project/project.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { ManageProjectsComponent } from './manage-projects/manage-projects.component';
 import { MyPersonnelProjectsComponent } from './my-personnel-projects/my-personnel-projects.component';
-import {ProjectRoutingModule} from './project-routing.module';
+import { ProjectRoutingModule } from './project-routing.module';
 
 
 @NgModule({
